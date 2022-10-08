ee29460af556155bd288adfef83cddc5fee1d213
Vitalii Novgorodov <fivcheg@gmail.com>
2022-10-08 15:07:54 +0300

    Excluded unnecessary

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,3 @@
-fun main(args: Array<String>) {
+fun main() {
     println("My first-app!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
 }
